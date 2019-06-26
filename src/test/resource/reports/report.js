8a7ad8383aa2c1c528a1b6d3d4e5ff1ebccd7a0d
sandeep12887 <sandeepraj.s87@gmail.com>
2019-06-26 14:10:01 +0530

    second addition

--- src/test/resource/reports/report.js
+++ src/test/resource/reports/report.js
@@ -49,36 +49,33 @@ formatter.match({
   "location": "Stepdefinition_Demo.user_launch_the_demo_application()"
 });
 formatter.result({
-  "duration": 11427269703,
+  "duration": 10677927874,
   "status": "passed"
 });
 formatter.match({
   "location": "Stepdefinition_Demo.user_enter_the_username_in_the_email_feild()"
 });
 formatter.result({
-  "duration": 295162955,
-  "status": "passed"
+  "duration": 6131441071,
+  "error_message": "java.lang.Exception\r\n\tat com.BaseAdactin.Base_Adactin.inputValuestoWebelement(Base_Adactin.java:109)\r\n\tat com.stepdefinition.Stepdefinition_Demo.user_enter_the_username_in_the_email_feild(Stepdefinition_Demo.java:29)\r\n\tat ✽.When User enter the username in the email feild(DemoAp.feature:6)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "Stepdefinition_Demo.user_enter_the_password_in_the_password_feild()"
 });
 formatter.result({
-  "duration": 142985933,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "Stepdefinition_Demo.user_click_on_Sign_in_button()"
 });
 formatter.result({
-  "duration": 3245811961,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "Stepdefinition_Demo.user_verify_the_project_name_in_the_logo()"
 });
 formatter.result({
-  "duration": 47824479,
-  "error_message": "org.junit.ComparisonFailure: expected:\u003cProject[]\u003e but was:\u003cProject[ name]\u003e\r\n\tat org.junit.Assert.assertEquals(Assert.java:115)\r\n\tat org.junit.Assert.assertEquals(Assert.java:144)\r\n\tat com.stepdefinition.Stepdefinition_Demo.user_verify_the_project_name_in_the_logo(Stepdefinition_Demo.java:47)\r\n\tat ✽.Then User verify the project name in the logo(DemoAp.feature:9)\r\n",
-  "status": "failed"
+  "status": "skipped"
 });
 });
